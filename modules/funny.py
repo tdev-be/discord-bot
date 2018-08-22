7ba304f329b340cd9d18a1f03536d7284b05a8b9
Quentin Tawry <quentin.tawry@haulogy.net>
2018-08-22 11:12:43 +0200

    add beer and tea timer commands

--- modules/funny.py
+++ modules/funny.py
@@ -3,6 +3,8 @@ import discord
 import logging
 import datetime
 import psutil
+import threading
+from asyncio import sleep
 from utils.db.datatype import *
 
 from config import LOGGING_CHANNEL
@@ -22,6 +24,26 @@ class funny:
         if member==None:
             member= ctx.author
         await ctx.send(f':pizza: Here is your pizza {member.mention}! :pizza:')
+
+    @commands.command(hidden=False)
+    async def beer(self, ctx, member: discord.Member=None):
+        '''Give me a beer'''
+        if member==None:
+            member= ctx.author
+        await ctx.send(f':beer: Here is your beer {member.mention}! :beer:')
+
+    @commands.command(hidden=False)
+    async def tea(self, ctx, timer=0):
+        '''Tea time'''
+        member = ctx.author
+        await sleep(timer*60)
+
+        await ctx.send(f'Your tea is ready {member.mention}!')
+        # async def sendDM(ctx, member):
+        #     print('TEST')
+        #     await ctx.send(f'Your tea is ready {member.mention}!')
+        # job = threading.Timer(timer, sendDM, (ctx, member))
+        # job.start()
 
 async def on_error(self, event, *args, **kwargs):
     e = discord.Embed(title='Event Error', colour=0xa32952)
